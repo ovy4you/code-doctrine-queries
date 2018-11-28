c162cefc03dfaefe078cccf6b7620718fa322e03
ovidiu.ciuca <ovidiu.ciuca@emag.ro>
2018-11-28 11:48:39 +0200

    code-doctrine-queries

---
+++ src/AppBundle/Controller/FortuneController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManager;
+
+class FortuneController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepageAction(Request $request)
+    {
+        $categoryRepository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Category');
+
+        $search = $request->query->get('q');
+        if ($search) {
+            $categories = $categoryRepository->search($search);
+        } else {
+            $categories = $categoryRepository->findAllOrdered();
+        }
+
+        return $this->render('fortune/homepage.html.twig',[
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category_show")
+     */
+    public function showCategoryAction($id)
+    {
+        $categoryRepository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Category');
+
+        $category = $categoryRepository->findWithFortunesJoin($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException();
+        }
+
+        $fortunesData = $this->getDoctrine()
+            ->getRepository('AppBundle:FortuneCookie')
+            ->countNumberPrintedForCategory($category);
+        $fortunesPrinted = $fortunesData['fortunesPrinted'];
+        $averagePrinted = $fortunesData['fortunesAverage'];
+        $categoryName = $fortunesData['name'];
+
+        return $this->render('fortune/showCategory.html.twig',[
+            'category' => $category,
+            'fortunesPrinted' => $fortunesPrinted,
+            'averagePrinted' => $averagePrinted,
+            'categoryName' => $categoryName
+        ]);
+    }
+}
---
+++ src/AppBundle/EventListener/BeforeRequestListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class BeforeRequestListener
+{
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $filter = $this->em
+            ->getFilters()
+            ->enable('fortune_cookie_discontinued');
+        $filter->setParameter('discontinued', false);
+    }
+}
